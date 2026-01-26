648286b1b707f3218e6e6318e6332343a1dde3e6
Bttaleb <btalebg@gmail.com>
2026-01-26 17:27:20 -0500

    First

---
+++ myGame/myGameTests/myGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  myGameTests.swift
+//  myGameTests
+//
+//  Created by Bassel Taleb on 1/26/26.
+//
+
+import Testing
+@testable import myGame
+
+struct myGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
